436abc4b425c4610a16ff4047e0fcfc6d9d56607
ulisse-fontanot <ulissef99@gmail.com>
2021-02-16 11:13:21 +0100

    fino corsi-senza testimonial/slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
 var app = new Vue({
   el: "#app",
   data:{
-    menu: ["Home","Pages","Courses","Features","Blog","Shop"]
+    menu: ["Home","Pages","Courses","Features","Blog","Shop"],
+    corsi: [
+      {
+        costo: 18.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 21.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 19.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 35.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 19.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 19.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 25.00,
+        nome: "The Acrylic Painting Academy"
+      },
+      {
+        costo: 22.00,
+        nome: "The Acrylic Painting Academy"
+      }
+    ]
   },
   methods: {
 
