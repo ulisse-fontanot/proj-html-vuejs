c6e221a58af6d14ee105fdc25cdb59795762a8d9
ulisse-fontanot <ulissef99@gmail.com>
2021-02-16 15:08:18 +0100

    ancora slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: "#app",
   data:{
+    counter: 0,
     menu: ["Home","Pages","Courses","Features","Blog","Shop"],
     corsi: [
       {
@@ -38,24 +39,31 @@ var app = new Vue({
     ],
     teacher: [
       {
+        src: "img/artist-testimonial-avatar-01.jpg",
         nome: "Florence Themes",
         materia: "/ Multimedia Admin"
       },
       {
+        src: "img/artist-testimonial-avatar-02.jpg",
         nome: "Mina Hallace",
         materia: "/ Freelancer"
       },
       {
+        src: "img/artist-testimonial-avatar-03.jpg",
         nome: "Madley Pandor",
         materia: "/ IT Specialist"
       },
       {
+        src: "img/artist-testimonial-avatar-04.jpg",
         nome: "Ajeje Brazorf",
         materia: "/ Train Passenger"
       }
     ]
   },
   methods: {
-
+    questo(indice){
+      this.counter = indice;
+      console.log(this.counter);
+    }
   }
 });
